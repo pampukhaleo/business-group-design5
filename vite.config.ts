97c72bbc92eae2a797e12875623f18c3bd2bdd07
pampukhaleo <lgpampukha@gmail.com>
2025-09-24 11:23:56 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/business-group-design5/',
   server: {
     host: "::",
     port: 8080,
